3baf59d7fc61d2a4a56ef9b4374c0a5ba6f03a41
Aman <aakashaman1770@gmail.com>
2020-09-12 01:37:59 +0530

    Updates

---
+++ precache-manifest.aaad3d21a816723b70a7f5c8dcb1a13a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d52bff5142ef4997f8be96e76dadbf29",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "8dbc1b080527631d1816",
+    "url": "/amazon-clone/static/css/main.7ce0a7bd.chunk.css"
+  },
+  {
+    "revision": "957ef97e1b6690e7050b",
+    "url": "/amazon-clone/static/js/2.98f381df.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/amazon-clone/static/js/2.98f381df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dbc1b080527631d1816",
+    "url": "/amazon-clone/static/js/main.b7279439.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  }
+]);
